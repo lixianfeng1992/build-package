994e8380b2bb482d4c6c0e8895a32f6cae63af5c
Allen Li <lixianfeng1992@gmail.com>
2019-07-21 12:43:00 +0800

    add travis

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,7 +4,7 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base.js');
   
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, '/Users/allenli/Workspace/Frontend/build-package/test/smoke/template/src/index/index.js'),
-    assert.equal(baseConfig.entry.search, '/Users/allenli/Workspace/Frontend/build-package/test/smoke/template/src/search/index.js')
+    assert.equal(baseConfig.entry.index.indexOf('build-package/test/smoke/template/src/index/index.js') > -1, true),
+    assert.equal(baseConfig.entry.search.indexOf('build-package/test/smoke/template/src/search/index.js') > -1, true)
   })
 });
